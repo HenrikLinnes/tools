f9a66cd2a518860fb0e69f9fb6a290ee811a2133
HenrikL <henrik.linnes@qvisten.no>
2022-10-23 23:49:30 +0200

    added temp file

--- fancy_tools/temp.py
+++ fancy_tools/temp.py
@@ -1,13 +1,36 @@
 import sys
 import os
+import glob
+import fileinput
 
-from fancy_tools.utils import fileContentReplace
+def findFiles(path, file):
+    found_files = []
+    for f in glob.glob("/%s/**/%s" %(path, file), recursive=True):
+        found_files.append(f)
+    return found_files
 
-path = "\\storage01.qvisten.no\rendertemp\kutoppen3\output\groomdata\q002.4\shots"
+def replace(found_files, old, new):
+    files_renamed = 0
+    for f in found_files:
+        temp_file = open(f, "r+")
+
+        for line in fileinput.input(f):
+            if old in line:
+                print("Match Found")
+                temp_file.write(line.replace(old, new))
+                files_renamed+=1
+            else:
+                print("Match Not Found")
+        temp_file.close()
+    return files_renamed
+
+
+
+path = "\\storage01.qvisten.no\rendertemp\kutoppen3\output\groomdata\q002.4\shots\q002.4_s004.0"
 file = "bernt\hair.usda"
-found_files = fileContentReplace.findFiles(path, file)
+found_files = findFiles(path, file)
 
 old = "__default"
 new = "__bigHair"
 
-fileContentReplace.replace(found_files, old, new)
+replace(found_files, old, new)
